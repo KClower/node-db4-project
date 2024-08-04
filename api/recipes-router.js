f3ab863d89b83f6fa013268b17278a56bbe81be5
KClower <kclower3@gmail.com>
2024-08-04 17:28:12 -0500

    Finished project

--- api/recipes-router.js
+++ api/recipes-router.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const RecipesModel = require('../api/recipes-model.js');
+const Recipes = require('../api/recipes-model.js');
 
 const router = express.Router();
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Recipes.getRecipeById(id)
+        .then(result => res.status(200).json(result))
 
+})
 
+// .then(recipe => {
+//     res.status(200).json(recipe)
+// })
 
-
-
+//.then(result => res.status(200).json(result))
 
 
 
